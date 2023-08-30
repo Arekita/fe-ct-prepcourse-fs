19bada6d941777a293de8eb2be2e0284390a28fb
Arekita <alejomamu2001mail.com>
2023-08-29 23:32:59 -0500

    second

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -4,37 +4,38 @@
 function sonIguales(x, y) {
    // Retorna true si "x" e "y" son iguales.
    // De lo contrario, retorna false.
-   // Tu código:
+   // Tu código: console.log(3 = 3);
+
 }
 
 function tienenMismaLongitud(str1, str2) {
    // Retorna true si los dos strings tienen la misma longitud.
    // De lo contrario, retorna false.
-   // Tu código:
+   // Tu codigo: console.log("Hola" = "Hola");
 }
 
 function menosQueNoventa(num) {
    // Retorna true si el argumento "num" es menor que noventa.
    // De lo contrario, retorna false.
-   // Tu código:
+   // Tu código: console.log(50 < 90);  
 }
 
 function mayorQueCincuenta(num) {
    // Retorna true si el argumento "num" es mayor que cincuenta.
    // De lo contrario, retorna false.
-   // Tu código:
+   // Tu código: console.log(90 > 50);
 }
 
 function esPar(num) {
    // Retorna true si "num" es par.
    // De lo contrario, retorna false.
-   // Tu código:
+   // Tu código: console.log(2)
 }
 
 function esImpar(num) {
    // Retorna true si "num" es impar.
    // De lo contrario, retorna false.
-   // Tu código:
+   // Tu código: console.log(3);
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
