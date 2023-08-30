046af6dff5f2c18655a69e8265b99e7c280cd932
Arekita <alejomamu2001mail.com>
2023-08-29 22:20:33 -0500

    ´first´

--- M03 JavaScript Fundamentos/Ejercicio 01.js
+++ M03 JavaScript Fundamentos/Ejercicio 01.js
@@ -5,22 +5,26 @@
 // por el correspondiente.
 
 // Crea una variable de tipo string.
-const nuevoString = null;
+const nuevoString = "hola";
 
 // Crea una variable de tipo number.
-const nuevoNumero = null;
+const nuevoNumero = var_numeroentero = 17;
 
 // Crea una variable de tipo boolean.
-const nuevoBoolean = null;
+const nuevoBoolean = var_estoyentendiendo = true
+var_estoyaburrido = false;
 
 // Resuelve el siguiente problema matemático.
-const nuevaResta = 10 - null === 3;
+const nuevaResta = 10 - null === 3
+console.log(10-7);
 
 // Resuelve el siguiente problema matemático.
-const nuevaMultiplicacion = 10 * null === 40;
+const nuevaMultiplicacion = 10 * null === 40
+console.log(10*4);
 
 // Resuelve el siguiente problema matemático.
-const nuevoModulo = 21 % 5 === null;
+const nuevoModulo = 21 % 5 === null
+console.log(21 % 5);
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
